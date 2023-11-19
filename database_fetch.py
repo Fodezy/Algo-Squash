01616c86fd0cf3f659aa931e1805f0ccec37f448
james-kidd <james.kidd02@hotmail.com>
2023-11-19 00:25:39 -0500

    added comments for readability

--- database_fetch.py
+++ database_fetch.py
@@ -1,46 +1,58 @@
 import json
 import mysql.connector
 
-#"id, email, availability, created, name, skill"
-
+# Function to load data from the server
 def LoadServer():
     try:
+        # Establish a connection to the MySQL database
         db_connection = mysql.connector.connect(
-        host="34.42.173.231",
-        user="root",
-        password="BIUouwbwi739372",
-        database="SquashNoFriends"
+            host="34.42.173.231",
+            user="root",
+            password="BIUouwbwi739372",
+            database="SquashNoFriends"
         )
         cursor = db_connection.cursor()
-        ### PLUG IN FUNCTIONS
-        extracted_raw_data = extract_data_query(cursor)
-        ### END OF functions
 
+        # Extract data from the database
+        extracted_raw_data = extract_data_query(cursor)
+
+        # Commit any changes to the database
         db_connection.commit()
     except mysql.connector.Error as err:
+        # Print any errors that occur during the connection or data extraction
         print(f"Error: {err}")
         return -1
     finally:
+        # Close the database connection
         if db_connection.is_connected():
             cursor.close()
             db_connection.close()
             print("MySQL connection is closed")
+            # Return the extracted data
             return extracted_raw_data
 
+# Function to extract data from the database
 def extract_data_query(cursor):
+    # Execute the SQL query to select data
     cursor.execute("SELECT id, availability, email FROM submissions")
 
+    # Fetch all the rows returned by the database
     result = cursor.fetchall()
-    data = {}  # Initialize as an empty dictionary
+    data = {}  # Initialize as an empty dictionary to store the data
+
+    # Process each row in the result
     for row in result:
         id, availability, email = row
+        # Convert the JSON string in 'availability' to a dictionary
         availability_dict = json.loads(availability)
-        data[id] = {  # Use id as the key
+        # Store each user's data with their 'id' as the key
+        data[id] = {
             'availability': availability_dict,
             'email': email
         }
+
+    # Return the processed data
     return data   
 
-
-#data = LoadServer()
-
+# Uncomment the line below to execute the LoadServer function and load data
+# data = LoadServer()
--- total_overlap_algo.py
+++ total_overlap_algo.py
@@ -2,12 +2,13 @@ from datetime import datetime, timedelta
 from itertools import combinations
 import random
 
-# Function to generate time slots
+# Function to generate time slots within a specified range
 def generate_times(start_time_str, end_time_str, fmt='%I:%M %p'):
     times = []
     start_time = datetime.strptime(start_time_str, fmt)
     end_time = datetime.strptime(end_time_str, fmt)
     
+    # Loop through the time range and add each hour to the list
     current_time = start_time
     while current_time <= end_time:
         times.append(current_time.strftime(fmt))
@@ -15,12 +16,14 @@ def generate_times(start_time_str, end_time_str, fmt='%I:%M %p'):
 
     return times
 
-# Function to find common availabilities between two users
+# Function to find common available time slots between two users
 def find_common_availabilities(user1, user2):
     common_availabilities = {}
+    # Check each day for overlapping times
     for day in user1['availability']:
         if day in user2['availability']:
             common_times = []
+            # Compare each time slot for both users
             for time in generate_times('8:00 AM', '8:00 PM'):
                 if user1['availability'][day].get(time, False) and user2['availability'][day].get(time, False):
                     common_times.append(time)
@@ -28,14 +31,16 @@ def find_common_availabilities(user1, user2):
                 common_availabilities[day] = common_times
     return common_availabilities
 
-# Calculate total overlapping hours
+# Calculate the total duration of overlapping time slots
 def calculate_duration(common_availabilities):
     return sum(len(times) for times in common_availabilities.values())
 
+# Find the best matches based on the maximum overlap in availability
 def find_best_matches(users):
     overlaps = {}
-    common_times_dict = {}  # To store common available times for each match
+    common_times_dict = {}  # Stores common available times for each match
 
+    # Compare each pair of users
     for (id1, user1), (id2, user2) in combinations(users.items(), 2):
         common_availabilities = find_common_availabilities(user1, user2)
         total_overlap = calculate_duration(common_availabilities)
@@ -45,38 +50,47 @@ def find_best_matches(users):
     matches = {}
     matched_indices = set()
 
+    # Find the best match for each user, avoiding duplicate matches
     while overlaps:
         best_match = max(overlaps, key=overlaps.get)
         matched_indices.update(best_match)
         matches[best_match] = overlaps[best_match]
 
-        # Remove matched users and their related pairs
+        # Remove already matched users from further consideration
         overlaps = {pair: time for pair, time in overlaps.items() if pair[0] not in matched_indices and pair[1] not in matched_indices}
 
-    return matches, common_times_dict  # Return both matches and common available times
+    return matches, common_times_dict  # Return both the matches and the common available times
 
+# Generate random users with random availability
 def generate_users(num_of_users, busy_schedule_ratio):
     grand_dict = {}
-    for user_id in range(1, num_of_users + 1):  # Adjusted to include num_of_users
+    # Create a user entry for each ID in the specified range
+    for user_id in range(1, num_of_users + 1):
         availability = {}
         days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-        times = generate_times('8:00 AM', '8:00 PM')  # Using the generate_times function for consistency
+        
+        # Use generate_times to create consistent time slots
+        times = generate_times('8:00 AM', '8:00 PM')  
         
         skill = ['Beginner', 'Intermediate', 'Advanced']
         
+        # Randomly assign availability for each time slot
         for day in days:
             availability[day] = {}
             for time in times:
                 availability[day][time] = random.choices([True, False], weights=[1, busy_schedule_ratio])[0]
         
+        # Add the user with their availability and skill level to the dictionary
         grand_dict[user_id] = {'availability': availability, 'skill': random.choice(skill)}
+    
     return grand_dict
 
 def main():
+    # Generate test users and find the best matches
     test_users = generate_users(120, 3)
-    best_matches, common_times_dict = find_best_matches(test_users)  # Get common times as well
+    best_matches, common_times_dict = find_best_matches(test_users)
 
-    # Print Results
+    # Print the results, including specific overlapping times
     for match, overlap in best_matches.items():
         print(f"Match between User {match[0]} and User {match[1]}: Overlap Duration: {overlap} hours")
         for day, times in common_times_dict[match].items():
