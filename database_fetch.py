3a600a144073368e1d7250698964585206b12afe
james-kidd <james.kidd02@hotmail.com>
2023-11-18 22:23:49 -0500

    function load database and queury extract data

--- database_fetch.py
+++ database_fetch.py
@@ -1,36 +1,47 @@
 import json
 import mysql.connector
 
-"id, email, availability, created, name, skill"
-
-##right now im just doing id=0 user to all users [1,n]
-def entity_user():
+#"id, email, availability, created, name, skill"
 
 def LoadServer():
     try:
         db_connection = mysql.connector.connect(
-        host="",
-        user="",
-        password="",
-        database=""
+        host="34.42.173.231",
+        user="root",
+        password="BIUouwbwi739372",
+        database="SquashNoFriends"
         )
         cursor = db_connection.cursor()
         ### PLUG IN FUNCTIONS
-        extract_data_queury(cursor)
+        extracted_raw_data = extract_data_query(cursor)
         ### END OF functions
 
         db_connection.commit()
     except mysql.connector.Error as err:
         print(f"Error: {err}")
+        return -1
     finally:
         if db_connection.is_connected():
             cursor.close()
             db_connection.close()
             print("MySQL connection is closed")
+            return extracted_raw_data
 
 def extract_data_query(cursor):
     cursor.execute("SELECT id, availability, skill FROM submissions")
+
     result = cursor.fetchall()
-    for x in result:
-        print(x)
-    return result
+    data = []
+    for row in result:
+        id, availability, skill = row
+        availability_dict = json.loads(availability)
+        data.append({
+            'id': id,
+            'availability': availability_dict,
+            'skill': skill
+        })
+    return data       
+
+
+data = LoadServer()
+print(data)
